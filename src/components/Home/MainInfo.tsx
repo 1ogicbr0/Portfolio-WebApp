386cdc66c18d6e18b738b849ac9e0e4940b89b27
Logic <shehryaar5@gmail.com>
2022-08-22 23:28:16 +0400

    Added some  animations using Gsap in HomePage/Main-Info

--- src/components/Home/MainInfo.tsx
+++ src/components/Home/MainInfo.tsx
@@ -1,36 +1,83 @@
 import styles from "./MainInfo.module.css";
-import Anime from "@mollycule/react-anime";
-
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 const MainInfo = () => {
+  const startRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+  gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to("#textStartIntro, #textStartName", {
+      scrollTrigger:{
+        trigger:"#textStartIntro, #textStartName",
+        start: "top center",
+        end: "+=300",
+        scrub:1,
+        pin:"#start",
+        toggleActions: "restart pause reverse pause",
+        markers:true,
+      },
+      translateY: -200,
+      rotateX:100,
+      opacity:0,
+      duration: 5,
+    });   
+  }, []);
 
   return (
     <div className={styles.main}>
-        <div className={styles.beforeStart}></div>
-      <div className={styles.start}>
-        <div className={styles.line}></div>
-        <div className={styles.info}>
-        <Anime
-        in
-        appear
-        duration={1000}
-        onEntering={{ translateY: [-20,0], opacity:[0,1]}}
-        onExiting={{ translateY: -20, opacity:0}}
-        easing="easeOutCubic"
-        >
-          GREAT
-        </Anime>
-        <div className={styles.intro} >
-          <span>$:~</span>
-          <span>whoami</span>
+      <div className={styles.beforeStart}></div>
+
+      <div className={styles.start} id="start">
+        <div className={styles.left}><div className={styles.line}></div></div>
+        <div className={styles.right}>
+        <div className={styles.info} >
+          <div className={styles.intro} id="textStartIntro" >
+            <span>$:~</span>
+            <span>whoami</span>
+          </div>
+          <div className={styles.name} id="textStartName" >
+            <span>Muhammad</span>
+            <span>Shehryaar</span>
+            <span>Khan</span>
+          </div>
+          <div className={styles.fieldWeb} id="textStartField1"> Creative Web Developer </div>
+          <div className={styles.fieldCyber} id="textStartField2"> Bug Bounty Hunter</div>
         </div>
-        <div className={styles.name}><span>Muhammad</span><span>Shehryaar</span><span>Khan</span></div>
-        <div className={styles.fieldWeb}> Creative Web Developer </div>
-        <div className={styles.fieldCyber}> Bug Bounty Hunter</div>
         </div>
       </div>
-      <div className={styles.mid}>Middle</div>
-      <div className={styles.end}>End</div>
+
+      <div className={styles.mid} id="mid">
+      <div className={styles.left}><div className={styles.line}></div></div>
+      <div className={styles.right}>
+        <div className={styles.info} id="text" ref={startRef}>
+          <div>From Pakistan to help the world</div>
+          <div>I did my Olevels from OPF College</div>
+          <div>and A-levels from Roots International School</div>
+          <div>
+            I am currently doing my bachelors in Cyber Security from Air
+            University
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div className={styles.end}>
+      <div className={styles.left}><div className={styles.line}></div></div>
+        <div className={styles.info}>
+          <div>Motive?</div>
+          <div>
+            The main thing that motivates me is to stuck in a problem and figure
+            most efficient way to get out of it
+          </div>
+          <div>Creating extraordinary websites using different tecnologies</div>
+          <div>
+            Learning about new technologies to find bugs and secure them
+          </div>
+        </div>
+      </div>
+      <div>END</div>
     </div>
   );
 };
--- src/components/UI/Layout/Navbar.tsx
+++ src/components/UI/Layout/Navbar.tsx
@@ -1,14 +1,15 @@
 import styles from './Navbar.module.css'
+import {NavLink} from 'react-router-dom'
 
 const Navbar:React.FC  = (props) => {
 
     return <div className={styles.navbar}>
         <div className={styles.logo}>.me()</div>
         <div className={styles.links}>
-        <div className={styles.navlink}>.is()</div>
-        <div className={styles.navlink}>.work()</div>
-        <div className={styles.navlink}>.about()</div>
-        <div className={styles.navlink}>.contact()</div>
+        <NavLink className={styles.navlink} to="/" >.is()</NavLink>
+        <NavLink className={styles.navlink} to="/work">.work()</NavLink>
+        <NavLink className={styles.navlink} to="/about">.about()</NavLink>
+        <NavLink className={styles.navlink} to="/contact">.contact()</NavLink>
         </div>
     </div>
 }
